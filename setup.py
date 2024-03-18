e62a92cc2155cecee71b04527d8d162cb7820d99
Sean Svihla <Sean.Svihla@lasp.colorado.edu>
2024-03-18 15:11:41 -0600

    Basic setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pcms',
+    version='1.0',
+    description='Phylogenetic Covariance Matrix Sparsification',
+    author='Sean Svihla',
+    packages=['pcms'],
+)
