aca1a4dbe4fbe35f960c5bdb88fc7e8154b40fbf
Sean Svihla <spsvihla@gmail.com>
2025-07-03 09:55:41 -0600

    FIx: sign error in haar.sparsify

--- src/haar/haar-sparsify.cpp
+++ src/haar/haar-sparsify.cpp
@@ -178,7 +178,7 @@ sparsify(Tree* tree)
             int sum = lsize + rsize;
             double tbl = tree->find_tbl(i);
             double lval = sqrt(static_cast<double>(rsize) / (lsize * sum));
-            double rval = sqrt(static_cast<double>(lsize) / (rsize * sum));
+            double rval = -1 * sqrt(static_cast<double>(lsize) / (rsize * sum));
             for(int j = 0; j < lsize; ++j)
             {
                 trlength_(n_idx) = trlength_[n_idx] + tbl;
