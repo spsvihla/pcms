9ce0da8a8f1178b1b791ed3ff9c962c3fe1e20bd
Sean Svihla <spsvihla@gmail.com>
2025-07-05 15:19:33 -0600

    Change: improve setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-import subprocess, os
-
+import subprocess
+import os
 
 DEBUG = os.getenv("DEBUG_BUILD", "0") == "1"
 
-# Set compile args depending on debug mode 
-extra_compile_args=['-std=c++17']
+extra_compile_args = ['-std=c++17']
 if DEBUG:
     print("DEBUG_BUILD set")
-    extra_compile_args += ["-g","-O0"]
+    extra_compile_args += ['-g', '-O0']
 else:
-    extra_compile_args += ["-O3"]
+    extra_compile_args += ['-O3']
 
-# Get GSL include and library paths using gsl-config
 def get_gsl_flags(flag):
-    return subprocess.check_output(["gsl-config", flag], text=True).strip().split()
+    return subprocess.check_output(['gsl-config', flag], text=True).strip().split()
 
 ext_modules = [
     Pybind11Extension(
         'pcms._tree',
-        [
+        sources=[
             'src/tree/tree.cpp',
             'src/tree/tree-dist.cpp',
             'src/tree/pybind11.cpp'
         ],
         include_dirs=['include/'],
-        library_dirs=get_gsl_flags("--libs") + get_gsl_flags("--prefix") + ["/usr/lib", "/usr/local/lib"],
-        libraries=[
-            "gsl", 
-            "gslcblas"
-        ],
+        library_dirs=get_gsl_flags('--libdir') + ['/usr/lib', '/usr/local/lib'],
+        libraries=['gsl', 'gslcblas', 'm'],
         extra_compile_args=extra_compile_args,
-        extra_link_args=get_gsl_flags("--libs"),
+        extra_link_args=['-g'],
+        language='c++',
+        define_macros=[('DEBUG', '1')] if DEBUG else []
     ),
     Pybind11Extension(
         'pcms._haar',
@@ -43,13 +40,16 @@ ext_modules = [
             'src/haar/pybind11.cpp'
         ],
         include_dirs=['include/'],
-        extra_compile_args=extra_compile_args       
+        extra_compile_args=extra_compile_args,
+        extra_link_args=['-g'],
+        language='c++',
+        define_macros=[('DEBUG', '1')] if DEBUG else []
     )
 ]
 
 setup(
     name='pcms',
-    version="0.1.0",
+    version='0.1.0',
     packages=find_packages(),
     cmdclass={'build_ext': build_ext},
     ext_modules=ext_modules,
