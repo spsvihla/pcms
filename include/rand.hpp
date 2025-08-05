cdc5e710b7ec6439d5bbdbdab593bc5d66db6e9f
Sean Svihla <spsvihla@gmail.com>
2025-08-05 13:23:32 -0600

    Add: rand_uniform_double and rand_uniform_int

--- include/rand.hpp
+++ include/rand.hpp
@@ -4,12 +4,38 @@
 // standard library includes
 #include <random>
 
+
+// Uniform(a, b)
+inline double
+rand_uniform_double(double a, double b, std::mt19937& rng)
+{
+    return a + ((rng() + 0.5) / (rng.max() + 1.0)) * (b - a);
+}
+
+// Uniform(a, b)
+inline double 
+rand_uniform_int(int a, int b, std::mt19937& rng)
+{
+    int range = b - a + 1;
+    int max = rng.max();
+    int bucket_size = (max + 1) / range;
+
+    int r;
+    do 
+    {
+        r = rng();
+    }
+    while(r >= bucket_size * range);
+
+    return r;
+}
+
 // Exponential(rate=lam)
 inline double 
 rand_exponential(double lam, std::mt19937& rng) 
 {
-    double U = (rng() + 0.5) * (1.0 / (rng.max() + 1.0));   // Uniform(0, 1)
-    return -std::log(U) / lam;
+    double u = rand_uniform_double(0.0, 1.0, rng);
+    return -std::log(u) / lam;
 }
 
 // Dirichlet(1,...,1)
