69a3c86ffb8543b22d57c6dbb9f2bf49bde0c52e
Sean Svihla <spsvihla@gmail.com>
2025-08-28 08:25:05 -0600

    Change: move rand_critical_beta_split to rand.hpp

--- src/tree/tree-dist.cpp
+++ src/tree/tree-dist.cpp
@@ -26,52 +26,6 @@
 
 namespace py = pybind11;
 
-// Constants
-constexpr double GAMMA = 0.57721566490153286060; // Euler-Mascheroni constant   
-
-
-// compute the nth harmonic number
-double 
-harmonic_number(int n) 
-{
-    double hn;
-    if(n <= 30)
-    {
-        hn = 0.0;
-        for(int k = 1; k <= n; ++k)
-        {
-            hn += 1.0 / k;
-        }
-    }
-    else
-    {
-        // error < 10e-8 for n > 30
-        hn = std::log(n) + GAMMA + 1.0 / (2 * n) - 1.0 / (12 * n * n);
-    }
-    return hn;
-}
-
-// sample the critical beta split distribution 
-inline double 
-rand_critical_beta_split(int n, std::mt19937& rng)
-{
-    double hn = harmonic_number(n - 1);
-    double factor = n / (2.0 * hn);
-
-    double u = rand_uniform_double(0.0, 1.0, rng);
-    double thresh = u / factor;
-    double cdf_val = 0.0;
-    for(int i = 1; i <= n - 1; ++i)
-    {
-        int denom = i * (n - i);
-        cdf_val += 1.0 / denom;
-        if(cdf_val >= thresh)
-        {
-            return i;
-        }
-    }
-    return n-1; // this line shouldn't be reached
-}
 
 inline void
 randomize_edge_lengths(Tree* tree, std::mt19937& rng)
